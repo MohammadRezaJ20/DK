d57b192642ebea2c312f656a4f23e3bd133a2704
MohammadRezaJ20 <mohammadreza240@gmail.com>
2026-07-30 04:39:23 +0330

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,6 +1,18 @@
+from pathlib import Path
 import yaml
 
 
-def load_config(path: str = "config.yaml") -> dict:
-    with open(path, "r", encoding="utf-8") as f:
+BASE_DIR = Path(__file__).resolve().parent
+
+
+def load_config(path: str | Path | None = None):
+    if path is None:
+        config_path = BASE_DIR / "config.yaml"
+    else:
+        config_path = Path(path)
+
+        if not config_path.is_absolute():
+            config_path = BASE_DIR / config_path
+
+    with open(config_path, "r", encoding="utf-8") as f:
         return yaml.safe_load(f)
